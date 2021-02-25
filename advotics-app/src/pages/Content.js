bd0e05b879f998a89e54cbbe204ef85b2617febe
16519111 <gregdimasbaskara@gmail.com>
2021-02-26 06:46:23 +0700

    Add Menu

--- advotics-app/src/pages/Content.js
+++ advotics-app/src/pages/Content.js
@@ -1,4 +1,5 @@
-import { AppBar, Toolbar, Typography, Button, Avatar, Paper, Grid } from '@material-ui/core';
+import React from "react"
+import { AppBar, Toolbar, Typography, Button, Avatar, Paper, Grid, Menu, MenuItem } from '@material-ui/core';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
 import Accordion from '@material-ui/core/Accordion';
 import AccordionSummary from '@material-ui/core/AccordionSummary';
@@ -49,17 +50,42 @@ const useStyles = makeStyles((theme) => ({
         marginBottom: "3%"
     },
     skuImage: {
-        width: "100px"
+        width: "100px",
+        marginRight: theme.spacing(1)
+    },
+    mkuPaper: {
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        marginTop: theme.spacing(1),
+        marginBottom: theme.spacing(1)
+    },
+    spacingPaper: {
+        marginLeft: theme.spacing(1),
+        marginRight: theme.spacing(1),
     }
   }));
 function DashboardContent() {
+    const [anchorEl, setAnchorEl] = React.useState(null);
+
+    const handleClick = (event) => {
+      setAnchorEl(event.currentTarget);
+    };
+  
+    const handleClose = () => {
+      setAnchorEl(null);
+    };
     const generateSKUItem = (product_name, image, price, quantity_sold) => {
         return (
-            <Paper>
+            <Paper className={classes.mkuPaper}>
                 <img src={image} alt="img-error" className={classes.skuImage}/>
-                <Typography>{product_name}</Typography>
-                <Typography>Rp {price}</Typography>
-                <Typography>{quantity_sold}</Typography>
+                <div>
+                    <Typography style={{ fontWeight: "bold" }}>{product_name}</Typography>
+                    <div className={classes.rowFlex}>
+                        <Typography variant="body2" color="textSecondary">Rp {price}</Typography>
+                        <Typography>{quantity_sold}</Typography>
+                    </div>
+                </div>
             </Paper>
         )
     }
@@ -70,28 +96,26 @@ function DashboardContent() {
                 <Typography variant="h4" color="textSecondary">
                     Dashboard
                 </Typography>
-                <Accordion>
-                    <AccordionSummary
-                    aria-controls="panel1a-content"
-                    id="panel1a-header"
-                    >
-                        <HiOutlineCalendar/>
-                        <Typography>
-                            Period
-                        </Typography>
-                        <Typography>
-                            11 September 2018 - 14 September 2018
-                        </Typography>
-                    </AccordionSummary>
-                    <AccordionDetails>
-                        <Typography>
-                            Period
-                        </Typography>
-                        <Typography>
-                            11 September 2018 - 14 September 2018
-                        </Typography>
-                    </AccordionDetails>
-                </Accordion>
+                <Paper className={classes.rowFlex} onClick={handleClick}>
+                    <HiOutlineCalendar className={classes.spacingPaper} />
+                    <Typography className={classes.spacingPaper}>
+                        Period
+                    </Typography>
+                    <Typography className={classes.spacingPaper}>
+                        11 September 2018 - 14 September 2018
+                    </Typography>
+                </Paper>
+                <Menu
+                    id="simple-menu"
+                    anchorEl={anchorEl}
+                    keepMounted
+                    open={Boolean(anchorEl)}
+                    onClose={handleClose}
+                >
+                    <MenuItem onClick={handleClose}>Profile</MenuItem>
+                    <MenuItem onClick={handleClose}>My account</MenuItem>
+                    <MenuItem onClick={handleClose}>Logout</MenuItem>
+                </Menu>
             </div>
             <Accordion className={classes.spacing}>
                 <AccordionSummary
@@ -170,6 +194,11 @@ function DashboardContent() {
                             </Typography>
                             <HiOutlineDotsVertical/>
                         </div>
+                        {dummySKUItem.map((item) => {
+                            return (
+                                generateSKUItem(item.product_name, item.image, item.price, item.quantity_sold)
+                            )
+                        })}
                     </Paper>
                 </Grid>
             </Grid>
