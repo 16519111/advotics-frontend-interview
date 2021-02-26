fe667cf78a340fc324d2971a55b88c52578ca9ec
16519111 <gregdimasbaskara@gmail.com>
2021-02-26 22:09:23 +0700

    Revert "Setting Calendar css"
    This reverts commit 6e24bfeb8d47386b9acf58761f47640fcc0b71c2.

--- advotics-app/src/components/Calendar.js
+++ advotics-app/src/components/Calendar.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import Calendar from 'react-calendar';
-import '../styles/Calendar.css';
+import 'react-calendar/dist/Calendar.css';
 
 export default function Filter() {
   const [value, onChange] = useState(new Date());
--- advotics-app/src/pages/Content.js
+++ advotics-app/src/pages/Content.js
@@ -58,7 +58,8 @@ const useStyles = makeStyles((theme) => ({
         display: "flex",
         flexDirection: "row",
         alignItems: "center",
-
+        marginTop: theme.spacing(1),
+        marginBottom: theme.spacing(1)
     },
     spacingPaper: {
         marginLeft: theme.spacing(1),
@@ -96,25 +97,26 @@ function DashboardContent() {
                 <Typography variant="h4" color="textSecondary">
                     Dashboard
                 </Typography>
-                <Accordion>
-                    <AccordionSummary
-                        aria-controls="panel1a-content"
-                        id="panel1a-header"
-                    >
-                        <div className={classes.mkuPaper}>
-                            <HiOutlineCalendar className={classes.spacingPaper}/>
-                            <Typography className={classes.spacingPaper}>
-                                Period
-                            </Typography>
-                            <Typography className={classes.spacingPaper}>
-                                11 September 2018 - 14 September 2018
-                            </Typography>
-                        </div>
-                    </AccordionSummary>
-                    <AccordionDetails>
+                <Paper className={classes.rowFlex} onClick={handleClick}>
+                    <HiOutlineCalendar className={classes.spacingPaper} />
+                    <Typography className={classes.spacingPaper}>
+                        Period
+                    </Typography>
+                    <Typography className={classes.spacingPaper}>
+                        11 September 2018 - 14 September 2018
+                    </Typography>
+                </Paper>
+                <Menu
+                    id="simple-menu"
+                    anchorEl={anchorEl}
+                    keepMounted
+                    open={Boolean(anchorEl)}
+                    onClose={handleClose}
+                >
+                    <MenuItem>
                         <Calendar />
-                    </AccordionDetails>
-                </Accordion>
+                    </MenuItem>
+                </Menu>
             </div>
             <Accordion className={classes.spacing}>
                 <AccordionSummary
